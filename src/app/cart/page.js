225595a2d93365aa7ff0f6a3aa2e277f41b2e070
Nicolashh <nicoo2003@gmail.com>
2025-03-21 12:10:41 -0300

    Se refactoriza el cartContext y se agrega la funcionalidad de guardar en localstore

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -4,17 +4,27 @@ import { ItemCart } from "@/components/ItemCart/ItemCart.jsx";
 import { useCart } from "../../hooks/useCart";
 import { CartContainer } from "@/components/CartContainer/CartContainer";
 import { DivideIcon } from "@heroicons/react/24/outline";
+import Spinner from "@/components/Spinner/Spinner";
 
 
 export default function Page() {
-  const { cart } = useCart();
+  const { cart, total, loading} = useCart();
 
 
- 
+  if (loading) return (<Spinner />)
+
+  //console.log("total:" , total)
 
   return (
-      
-      <CartContainer cart={cart}/>
+    <div className="w-full flex flex-col items-center gap-8 p-8 ">
+
+    <CartContainer cart={cart} />
+
+    <h2 className="text-2xl font-bold text-gray-800 mt-10">
+      Total: <span className="text-green-600">${total}</span>
+    </h2>
+  </div>
+  
 
   );
 }
--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -1,20 +1,39 @@
 'use client';
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CartContext = createContext()
 
 export function CartProvider ({children}){
     const [cart, setCart] = useState([])
+    const [total, setTotal] = useState(0)
+    const [loading, setLoading] = useState(true)
+
+      // Guardar el carrito en localStorage
+    useEffect(() => {
+      const savedCart = localStorage.getItem('cart');
+      if (savedCart) {
+        setCart(JSON.parse(savedCart));
+      }
+
+      setLoading(false)
+    }, []);
+
+    useEffect(() => {
+      localStorage.setItem('cart', JSON.stringify(cart));
+      totalAmount(); 
+    }, [cart]);
 
     const addToCart = (product) => {
 
-      const elementInCart = cart.find(item => item._id === product._id)
+      const elementInCart = cart.findIndex(item => item._id === product._id)
 
-      if(elementInCart){
-          elementInCart.quantity += 1
-          return
+      if(elementInCart >= 0){
+        const newCart = [...cart]
+        newCart[elementInCart].quantity += 1
+        setCart(newCart)
+        return
       }
 
       setCart([...cart, { ...product, quantity : 1 }]);
@@ -22,15 +41,21 @@ export function CartProvider ({children}){
     };
 
     const reduceFromCart = (product) => {
-      const elementInCart = cart.find(item => item._id === product._id)
+      const elementInCart = cart.findIndex(item => item._id === product._id)
 
-      if (!elementInCart) return
+      //console.log(elementInCart)
 
-      if(elementInCart.quantity === 1){
-        const newCart = [...cart].filter(item => (item !== elementInCart))
+      if (elementInCart < 0) return
+
+      //console.log("Quantity: ", cart[elementInCart].quantity)
+
+      if(cart[elementInCart].quantity === 1){
+        const newCart = [...cart].filter((_, index) => (index !== elementInCart))
         setCart(newCart)
       }else{
-        elementInCart.quantity -= 1
+        const newCart = [...cart]
+        newCart[elementInCart].quantity -= 1
+        setCart(newCart)
       }
     }
 
@@ -40,12 +65,20 @@ export function CartProvider ({children}){
       if (!elementInCart) return 
 
       const newCart = [...cart].filter(item => (item !== elementInCart))
-      //newCart.remove(elementInCart)
       setCart(newCart)
     }
 
+    const totalAmount = () => {
+      const total = cart.reduce((accumulator, current) => accumulator + (current.quantity * current.price), 0);
+      setTotal(total)
+    }
+
+    // useEffect(() => {
+    //   totalAmount()
+    // }, [cart])
+
     return (
-        <CartContext.Provider value={{ cart, setCart, addToCart, reduceFromCart, removeFromCart }}>
+        <CartContext.Provider value={{ cart, setCart, addToCart, reduceFromCart, removeFromCart, total, loading }}>
           {children}
         </CartContext.Provider>
       );
