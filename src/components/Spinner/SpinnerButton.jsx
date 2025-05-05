9b3a6a06b31d7b4d7155e72275728eb4078795c9
Nicolashh <nicoo2003@gmail.com>
2025-05-05 11:41:21 -0300

    Se agregan el componente SpinnerButton

---
+++ src/components/Spinner/SpinnerButton.jsx
@@ -0,0 +1,7 @@
+const SpinnerButton = () => {
+    return (
+        <div className="animate-spin rounded-full h-8 w-8 border-b-4 border-gray-900"></div>
+    );
+};
+
+export default SpinnerButton;
