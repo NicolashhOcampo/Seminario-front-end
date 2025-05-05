10fff1a89b20445253a9a49dfe6ebe032da5ad67
Nicolashh <nicoo2003@gmail.com>
2025-05-05 11:41:55 -0300

    Se agregan estilos a la pagina ForgotPassword

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,41 +1,49 @@
 import config from "@/config/app.config";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react"
 import toast, { Toaster } from "react-hot-toast"
+import SpinnerButton from "../Spinner/SpinnerButton";
 
 export const ForgotPassword = () => {
     const [email, setEmail] = useState("")
+    const [loading, setLoading] = useState(false)
     const router = useRouter()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         try {
-          const response = await fetch(`${config.urlHost}/api/auth/forgotPassword`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({email}),
-          });
-    
-          if (!response.ok){
-            if (response.status === 404){
-                toast.error("Usuario no encontrado");
-                return
+            setLoading(true)
+            const response = await fetch(`${config.urlHost}/api/auth/forgotPassword`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email }),
+            });
+
+            if (!response.ok) {
+                if (response.status === 404) {
+                    toast.error("No se encontró una cuenta asociada a ese correo electrónico.");
+                    return
+                }
+                else {
+                    throw new Error("Error inesperado")
+                }
+
             }
             else {
-                throw new Error("Error inesperado")
+                toast.success("Hemos enviado un enlace de recuperación a tu correo electrónico.")
             }
-            
-          }
-          else{
-            router.push("/login")
-          }
 
-          
-        } catch (err) {
-          //console.log(err)
+
+        }catch (err) {
+            console.error(err);
+            toast.error("Ocurrió un error al intentar recuperar la contraseña.");
         }
-      };
+        finally {
+            setLoading(false)
+        }
+    };
 
     return (
         <>
@@ -43,14 +51,14 @@ export const ForgotPassword = () => {
             <h2 className="text-[2rem] text-white mb-6 text-shadow text-lg font-bold">Recuperar Contraseña</h2>
             <form id="loginForm"
                 onSubmit={handleSubmit}
-                className="bg-[rgb(31,29,29)] w-2/5 p-8 grid grid-cols-2 gap-2 text-white rounded-md shadow-[0_4px_10px_rgba(0,0,0,0.3)]"
+                className="bg-[rgb(31,29,29)] w-9/10 md:w-2/5 max-w-120 p-8 flex flex-col items-center text-white rounded-md shadow-[0_4px_10px_rgba(0,0,0,0.3)]"
             >
 
-                <div className="col-span-2">
+                <div className="w-full">
                     <label className={"self-start text-base font-bold mb-[0.3rem]"}>Ingrese su Email</label>
                     <input
                         value={email}
-                        onChange={(e)=> setEmail(e.target.value)}
+                        onChange={(e) => setEmail(e.target.value)}
                         type="email"
                         id="email"
                         required
@@ -60,14 +68,15 @@ export const ForgotPassword = () => {
 
                 <button
                     type="submit"
-                    className="col-span-full text-base font-bold text-white bg-[#007acc] cursor-pointer transition-[background] duration-[0.3s] ease-[ease-in-out] p-[0.8rem] rounded-[0.3rem] border-none hover:bg-[#005f99]"
+                    disabled={loading}
+                    className="flex justify-center items-center col-span-full mt-4 w-30 h-10 text-base font-bold text-white bg-[#007acc] cursor-pointer transition-[background] duration-[0.3s] ease-[ease-in-out] rounded-[0.3rem] border-none hover:bg-[#005f99]"
                 >
-                    Enviar email
+                    {loading ? (<SpinnerButton />) : <p>Enviar Email</p>}
                 </button>
 
-                <a href="/login" className="no-underline col-span-2 text-white text-[0.8rem] text-center transition-[color] duration-[0.2s] ease-[ease-in-out] cursor-pointer mt-4 hover:text-[#007acc]">
+                <Link href="/login" className="no-underline col-span-2 text-white text-[0.8rem] text-center transition-[color] duration-[0.2s] ease-[ease-in-out] cursor-pointer mt-4 hover:text-[#007acc]">
                     Iniciar Sesion
-                </a>
+                </Link>
 
             </form>
         </>
