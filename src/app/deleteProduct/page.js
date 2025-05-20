659b4d8148dc1428964ac21315da94939c1a650b
ElPacho21 <rechefacundo1@gmail.com>
2025-05-19 22:49:26 -0300

    .

--- src/app/deleteProduct/page.js
+++ src/app/deleteProduct/page.js
@@ -2,10 +2,6 @@
 
 import { AdminGuard } from "@/components/AdminGuard/AdminGuard";
 import DeleteProductForm from "@/components/DeleteProductForm/DeleteProductForm";
-import Spinner from "@/components/Spinner/Spinner";
-import { useUser } from "@/context/UserContext";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Page() {
 
